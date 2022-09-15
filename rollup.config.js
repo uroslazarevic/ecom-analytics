7d659e7118b4aa6787b6cce69e7e477d74f657a0
Uroš Lazarević <enlision@gmail.com>
2022-09-15 23:45:26 +0200

    Remove esm from build

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,6 @@ export default [
   {
     input: "./src/index.ts",
     output: {
-      file: "./lib/index.esm.js",
-      format: "esm",
-    },
-    plugins: [
-      typescript({
-        tsconfigOverride: {
-          exclude: ["**/__tests__", "**/*.spec.ts"], // exclude spec file from build in lib dir
-        },
-      }),
-    ],
-  },
-  {
-    input: "./src/index.ts",
-    output: {
       file: "./lib/index.js",
       format: "cjs",
     },
